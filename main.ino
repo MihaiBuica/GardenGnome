63d86860730b5b257914b3012973e4c7bfea6d65
Mihai Buica <48243792+MihaiBuica@users.noreply.github.com>
2022-05-30 22:33:51 +0200

    Added simulation code on git. Running on esp32

---
+++ main.ino
@@ -0,0 +1,507 @@
+#include <WiFi.h>
+#include <Wire.h>
+
+#define SIMULATION_MODE
+// #define SEND_IFTTT_PKG
+#define PROCESS_IMG_CORE0
+#define PROCESS_SENSOR_CORE1
+//#define WOKWI_ENV
+
+#ifndef SIMULATION_MODE
+#include "DHTesp.h"
+#endif
+
+#ifdef WOKWI_ENV
+#define WLAN_SSID "Wokwi-GUEST"
+#define WLAN_PSWD ""
+#else
+#define WLAN_SSID "JumiWifi"
+#define WLAN_PSWD "93992032587598973862"
+#endif
+
+#define RAIN_PREDICTION
+ 
+#define AIR_PRESSURE  950
+#define CITY_ALTITUDE 520
+ 
+#define TARGET_TIMESTAMP 10
+#define DELAY_SEC        2
+ 
+#define RETRIES   5
+#define DAILY_CNT 24
+ 
+#define IOT_STATUS int
+#define IOT_SUCCESS 0
+#define IOT_SERVER_ERR -1
+#define IOT_SENSOR_ERR -2
+#define IOT_WIFI_ERR   -3
+ 
+// Tasks from the cores
+#ifdef PROCESS_SENSOR_CORE1
+TaskHandle_t sensorsCore;
+#endif
+#ifdef PROCESS_IMG_CORE0
+TaskHandle_t imageCore;
+#endif
+ 
+struct wifi_data
+{
+  const char* ssid     = WLAN_SSID;
+  const char* password = WLAN_PSWD;
+};
+// WiFi credentials
+struct wifi_data wifi;
+ 
+struct ifttt_data
+{
+  const char* resource = "/trigger/garden_dwarf_trigger/with/key/lZ44wiIR-3LIjEYrnmqgqUEIvlh9XDqyxh5MulgpYEb";
+  const char* server = "maker.ifttt.com";
+};
+// IFTTT resource
+struct ifttt_data ifttt;
+ 
+// Time to sleep
+uint64_t uS_TO_S_FACTOR = 1000000; // Conversion factor for micro seconds to seconds
+// sleep for 30 minutes = 1800 seconds
+uint64_t TIME_TO_SLEEP = 1;
+
+#ifndef SIMULATION_MODE
+const int DHT_PIN = 15;
+#endif
+ 
+int count_daily = 0;
+struct measurements
+{
+  int current;
+  int prevAvg;
+  int prediction;
+};
+struct daily_data
+{
+  int tempCurrent;
+  int humidityCurrent;
+  struct measurements temp;
+  struct measurements humidity;
+  int    tempRaw[DAILY_CNT];
+  int    humidityRaw[DAILY_CNT];
+  String rainPrediction;
+};
+
+#ifndef SIMULATION_MODE
+struct sensors
+{
+  DHTesp dhtSensor;
+};
+struct sensors availSensors;
+#endif
+
+struct daily_data dailyData;
+ 
+void setup()
+{
+  IOT_STATUS status = IOT_SUCCESS;
+  Serial.begin(115200);
+  status = initWifi();
+  if (IOT_SUCCESS != status)
+  {
+      Serial.println("Error occured during WIFI connection");
+      Serial.println("########");
+  };
+#ifndef SIMULATION_MODE
+  // Sensors setup
+  if (IOT_SUCCESS == status)
+  {
+    availSensors.dhtSensor.setup(DHT_PIN, DHTesp::DHT22);
+  }
+#endif
+
+  Serial.println("Setup...Success");
+ 
+  // Cores init
+#ifdef PROCESS_IMG_CORE0
+  // Image Core tasks
+  xTaskCreatePinnedToCore(
+    ImageCoreLoop, /* Function to implement the task */
+    "imageCore", /* Name of the task */
+    10000,  /* Stack size in words */
+    NULL,  /* Task input parameter */
+    1,  /* Priority of the task */
+    &imageCore,  /* Task handle. */
+    0); /* Core where the task should run */
+#endif
+ 
+#ifdef PROCESS_SENSOR_CORE1
+  // Sensors Core Task
+  xTaskCreatePinnedToCore(
+      SensorsCoreLoop, /* Function to implement the task */
+      "sensorsCore", /* Name of the task */
+      10000,  /* Stack size in words */
+      NULL,  /* Task input parameter */
+      1,  /* Priority of the task */
+      &sensorsCore,  /* Task handle. */
+      1); /* Core where the task should run */
+#endif
+}
+ 
+void SensorsCoreLoop( void * parameter)
+{
+  IOT_STATUS status = IOT_SUCCESS;
+ 
+  while(1)
+  {
+    // Core sanity check
+    Serial.print("SensorsCoreLoop() running on core ");
+    Serial.println(xPortGetCoreID());
+ 
+    status = runSensorsProcessing();
+ 
+    if (IOT_SUCCESS != status)
+    {
+      Serial.print("runSensorsProcessing() problem ... sleeping");
+      delay(3000);
+    }
+   
+    // Sleep
+    //delay(1000);
+  }
+ 
+  Serial.println("SensorsCoreLoop() ending... ");
+}
+ 
+void ImageCoreLoop( void * parameter)
+{
+    while(1)
+    {
+      // Core sanity check
+      Serial.print("ImageCoreLoop() running on core ");
+      Serial.println(xPortGetCoreID());
+      delay(2000);
+    }
+  Serial.println("ImageCoreLoop() ending...");
+}
+ 
+void loop()
+{
+#ifndef PROCESS_SENSOR_CORE1
+  // Core sanity check
+  Serial.print("loop() running on core ");
+  Serial.println(xPortGetCoreID());
+  runSensorsProcessing();
+#endif
+}
+ 
+IOT_STATUS runSensorsProcessing()
+{  
+  IOT_STATUS status = IOT_SUCCESS;
+ 
+  // Sensors stuff
+  status = getDHTSensorData((void*)&dailyData);
+  if (IOT_SUCCESS == status)
+  {
+    printDHTSensorData((void*)&dailyData);
+  }
+ 
+  // Aggregate sensor new values to stored ones
+  dailyData.tempRaw[count_daily] = dailyData.temp.current;
+  dailyData.humidityRaw[count_daily] = dailyData.humidity.current;
+ 
+  count_daily++;
+ 
+  // Compute the prediction for today's avg
+  status = predictValues((void*)&dailyData);
+ 
+#ifdef RAIN_PREDICTION
+  // Calculate raining chances
+  status = rainPrediction((void*)&dailyData);
+#endif
+ 
+  // Compute yesterday's values and reset the counter
+  if (DAILY_CNT == count_daily)
+  {
+    int i, sumTemp = 0, sumHum = 0;
+    for (i = 0; i < DAILY_CNT; i++)
+    {
+      sumTemp += dailyData.tempRaw[i];
+      sumHum += dailyData.humidityRaw[i];
+    }
+    dailyData.temp.prevAvg = sumTemp / DAILY_CNT;
+    dailyData.humidity.prevAvg = sumHum / DAILY_CNT;
+    count_daily = 0;
+  }
+ 
+  Serial.println(count_daily);
+ 
+  // Check if the hour is the set one
+  if (TARGET_TIMESTAMP == count_daily)
+  {
+#   ifdef SEND_IFTTT_PKG
+    if (IOT_SUCCESS == status)
+    {
+      Serial.println("IFTTT send message");
+      status = makeIFTTTRequest((void*)&dailyData);
+    }
+#   endif
+  }
+ 
+  // Error checking
+  if (IOT_SUCCESS != status)
+  {
+    //return;
+  }
+ 
+  // Sleep - ms
+  delay(DELAY_SEC * 1000);
+ 
+  return status;
+}
+ 
+// Linear interpolation
+IOT_STATUS predictValues(void *daily)
+{
+  IOT_STATUS status = IOT_SUCCESS;
+  // struct daily_data* data = (struct daily_data*) daily;
+ 
+  // int x0, y0Temp, x1, y1Temp, x, yTemp, y0Hum, y1Hum, yHum;
+  // if (count_daily > 0)
+  // {
+  //   x0 = 0;
+  //   y0Temp = data->tempRaw[x0];
+  //   y0Hum = data->humidityRaw[x0];
+  //   x1 = count_daily - 1;
+  //   y1Temp = data->tempRaw[x1];
+  //   y1Hum = data->humidityRaw[x1];
+  // }
+  // x = count_daily;
+  // Linear interpolation formula
+  // y = ((y0 * (x1 - x)) + (y1 * (x - x0))) / (x1 - x0)
+  // yTemp = ((y0Temp * (x1 - x)) + (y0Temp * (x1 - x))) / (x1 - x0);
+  // yHum = (y0Hum * (x1 - x)) / (x1 - x0);
+ 
+ 
+  return IOT_SUCCESS;
+}
+ 
+// Zambretti algorithm
+IOT_STATUS rainPrediction(void *daily)
+{
+  IOT_STATUS status = IOT_SUCCESS;
+  struct daily_data* data = (struct daily_data*) daily;
+  data->rainPrediction = String("N/A");
+#ifdef RAIN_PREDICTION
+  int Z, P0;
+  String forecasetPredictionText;
+  int temp = data->temp.current;
+  // 1. atmospheric pressure reduced to sea level
+  P0 = AIR_PRESSURE * (pow( 1 - ((0.0065 * CITY_ALTITUDE) / (temp + 0.0065 * CITY_ALTITUDE + 273.15)), -5.257));
+ 
+  // 2. Compute pressure trend - steady
+  Z = 144 - 0.13 * P0;
+ 
+  // 3. Adjust Z for wind direction: Northerly winds
+  Z += 1;
+ 
+  // 4. Adjust Z for the season:
+  Z += 1;
+ 
+  // 5. Lookup for the forecast
+  switch(Z)
+  {
+    case 9:
+      forecasetPredictionText = "Very Unsettled, Rain";
+      break;
+    case 10:
+      forecasetPredictionText = "Settled Fine";
+      break;
+    case 11:
+      forecasetPredictionText = "Fine Weather";
+      break;
+    case 12:
+      forecasetPredictionText = "Fine, Possibly Showers";
+      break;
+    case 13:
+      forecasetPredictionText = "Fairly Fine, Showers Likely";
+      break;
+    case 14:
+      forecasetPredictionText = "Showery, Bright Intervals";
+      break;
+    case 15:
+      forecasetPredictionText = "Changeable, Some Rain";
+      break;
+    case 16:
+      forecasetPredictionText = "Unsettled, Rain at Times";
+      break;
+    case 17:
+      forecasetPredictionText = "Rain at Frequent Intervals";
+      break;
+    case 18:
+      forecasetPredictionText = "Very Unsettled, Rain";
+      break;
+    case 19:
+      forecasetPredictionText = "Stormy, Much Rain";
+      break;
+    case 20:
+      forecasetPredictionText = "Settled Fine";
+      break;
+    case 21:
+      forecasetPredictionText = "Becoming Fine";
+      break;
+    default:
+      forecasetPredictionText = "Settled Fine";
+      break;
+  }
+  data->rainPrediction = forecasetPredictionText;
+  //Serial.println("Forecast: " + forecasetPredictionText);  
+#endif
+  return status;
+}
+ 
+#ifdef SIMULATION_MODE
+IOT_STATUS getDHTSensorData(void *daily)
+{
+  struct daily_data* data = (struct daily_data*) daily;
+ 
+  data->temp.current = 24;
+  data->humidity.current = 40;
+  int tempCurrent = 24;
+  int humidityCurrent = 40; 
+   Serial.println("Temp: " + String(tempCurrent, 2) + "°C");
+  Serial.println("Humidity: " + String(humidityCurrent, 1) + "%");
+  Serial.println("+++");
+  return IOT_SUCCESS;
+}
+#else
+IOT_STATUS getDHTSensorData(void *daily)
+{
+  struct daily_data* data = (struct daily_data*) daily;
+ 
+  data->temp.current = availSensors.dhtSensor.getTempAndHumidity().temperature;
+  data->humidity.current = availSensors.dhtSensor.getTempAndHumidity().humidity;
+  
+  return IOT_SUCCESS;
+}
+#endif
+
+void printDHTSensorData(void *daily)
+{
+  struct daily_data* data = (struct daily_data*) daily;
+  Serial.println("Temp: " + String(data->temp.current, 2) + "°C");
+  Serial.println("Humidity: " + String(data->humidity.current, 1) + "%");
+  Serial.println("---");
+}
+ 
+// Make an HTTP request to the IFTTT web service
+IOT_STATUS makeIFTTTRequest(void *daily)
+{
+  struct daily_data* data = (struct daily_data*) daily;
+ 
+  String current, yesterday, forecast;
+ 
+  current = String(data->temp.current, 2) + "°C; " + String(data->humidity.current, 1) + "%";
+  yesterday = String(data->temp.prevAvg, 2) + "°C; " + String(data->humidity.prevAvg, 1) + "%";
+  forecast = data->rainPrediction;
+  // Temperature in Celsius
+  String jsonObject = String("{\"value1\":\"") + current + "\",\"value2\":\""
+                      + yesterday + "\",\"value3\":\"" + forecast + "\"}";
+ 
+  if (IOT_SUCCESS != sendIFTTTRequest(jsonObject))
+  {
+    Serial.println("Error occured during IFTTT communication");
+    Serial.println("########");
+    return IOT_SERVER_ERR;
+  }
+  else
+  {
+    Serial.println("IFTTT communication succeed");
+  }
+ 
+  return IOT_SUCCESS;
+}
+ 
+IOT_STATUS sendIFTTTRequest(String jsonObject)
+{
+  Serial.print("Connecting to ");
+  Serial.print(ifttt.server);
+ 
+  WiFiClient client;
+ 
+  int retries = RETRIES;
+ 
+  while(!!!client.connect(ifttt.server, 80) && (retries-- > 0))
+  {
+    Serial.print(".");
+  }
+  Serial.println();
+ 
+  if(!!!client.connected())
+  {
+ 
+    Serial.println("Failed to connect...");
+    return IOT_SERVER_ERR;
+  }
+ 
+  Serial.print("Request resource: ");
+  Serial.println(ifttt.resource);
+ 
+  // Send request to IFTTT server
+  client.println(String("POST ") + ifttt.resource + " HTTP/1.1");
+  client.println(String("Host: ") + ifttt.server);
+  client.println("Connection: close\r\nContent-Type: application/json");
+ 
+  client.print("Content-Length: ");
+  client.println(jsonObject.length());
+  client.println();
+  client.println(jsonObject);
+ 
+  int timeout = RETRIES * 10;
+  while(!!!client.available() && (timeout-- > 0))
+  {
+    delay(100);
+  }
+ 
+  if(!!!client.available())
+  {
+    Serial.println("No response...");
+    return IOT_SERVER_ERR;
+  }
+ 
+  while(client.available())
+  {
+    Serial.write(client.read());
+  }
+ 
+  Serial.println("\nClosing connection...");
+  Serial.println("\n##################################\n");
+  client.stop();
+ 
+  return IOT_SUCCESS;
+}
+ 
+// Establish a Wi-Fi connection with your router
+IOT_STATUS initWifi()
+{
+  Serial.print("Connecting to: ");
+  Serial.print(wifi.ssid);
+ 
+  WiFi.begin(wifi.ssid, wifi.password);
+ 
+  int timeout = 10 * 4; // 10 seconds
+ 
+  while(WiFi.status() != WL_CONNECTED && (timeout-- > 0))
+  {
+    delay(250);
+    Serial.print(".");
+  }
+  Serial.println("");
+ 
+  if(WiFi.status() != WL_CONNECTED) {
+    Serial.println("Failed to connect, going back to sleep");
+    return IOT_WIFI_ERR;
+  }
+ 
+  Serial.print("WiFi connected in: ");
+  Serial.print(millis());
+  Serial.print(", IP address: ");
+  Serial.println(WiFi.localIP());
+ 
+  return IOT_SUCCESS;
+}
